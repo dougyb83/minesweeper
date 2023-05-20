818ba6d9e2a774ba731db1d6f8dc70ceea27ff69
dougyb83 <>
2023-05-20 17:33:09 +0000

    Add JS script

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+// function openGame() {
+//     window.location.href = "game.html";
+// }
+
+// let tileCount = 0;
+
+// $("#8x8").click(function() {
+//     tileCount = 8;
+//     // openGame();
+//     $("#8x8").css("color", "red");
+// });
+
+$("p").on("click", function () {
+
+    $("p").css("color", "red");
+
+}); 
+
